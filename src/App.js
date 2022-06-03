9bf7a740ffb4aee141c51cf9e7f93c1ad7c8f9f7
Alberto Aimar <alaimar@gmail.com>
2022-06-04 01:32:08 +0200

    Added HI AWS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 
-          Hi there!!!!!!!!
+          Hi there AWS !!!!!!!!
         </p>
         <a
           className="App-link"
