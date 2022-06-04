34ce6b7de0d8bc441df4903ff9f29b28fd552b06
Alberto Aimar <alaimar@gmail.com>
2022-06-04 11:55:52 +0200

    removed AWS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. 
-          Hi there AWS wowowowow  !!!!!!!!
+          Hi there wowowowow  !!!!!!!!
         </p>
         <a
           className="App-link"
